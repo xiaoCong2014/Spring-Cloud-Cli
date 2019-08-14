e622a70ae1900832f597b2f1f117979fcd47b2d5
1149023208@qq.com <1149023208@qq.com>
2019-08-14 18:15:47 +0800

    [init] 自己本地的配置,如配置数据库

--- vue-front/src/views/dashboard/index.vue
+++ vue-front/src/views/dashboard/index.vue
@@ -1,9 +1,34 @@
 <template>
   <div class="dashboard-container">
-    <a href="https://github.com/Heeexy/SpringBoot-Shiro-Vue" target="_blank"><img style="margin: 40px"
-      src="http://ots7yt7am.bkt.clouddn.com/file/github.png"></a>
+
+    <!--
+        图片链接 404 了
+        <img style="margin: 40px"
+            src="http://ots7yt7am.bkt.clouddn.com/file/github.png">
+    -->
+
+    <a href="https://github.com/Heeexy/SpringBoot-Shiro-Vue" target="_blank">
+        点此打开新链接 -- SpringBoot-Shiro-Vue 项目
+    </a>
+
+    </br>
+
+    <a href="https://github.com/Heeexy/SpringBoot-Shiro-Vue" target="_blank">
+        <span>包了 span </span>
+    </a>
+
+    </br>
+
+    <a href="https://github.com/Heeexy/SpringBoot-Shiro-Vue" target="_blank">
+        <a href="https://github.com/Heeexy/SpringBoot-Shiro-Vue" target="_blank">包了 a </a>
+    </a>
+
+    </br>
+
   </div>
+
 </template>
+
 <script>
   export default {
     name: 'dashboard',
